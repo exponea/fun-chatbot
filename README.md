04d5d3184ef0cff8f54e64131eeae3f45a57425e
Jozo Kovac <jozo.kovac@infinario.com>
2016-08-08 23:40:27 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# fun-chatbot
-Web Chatbot assembled for fun
+
+# Joana the Chatbot
+
+BusinessInsider has publsihed [an article](http://www.businessinsider.com/chatbots-explained-why-businesses-should-be-paying-attention-to-the-chatbot-revolution-2016-7) published an article about chatbots and mentioned Exponea among enabling technology. We indeed have integrated our marketing automation with Facebook Messenger API to enable chatbots but that's not what people want. They want a real chatbot. 
+
+So we have built it, pardon - her. Her name is Joana and is a grand daughter of Elisa born in 1966. She has some skills of psychoterapist and arguing with her is a bit tricky ..
+Have fun. 
+
+Try it online(https://conversation.exponea.com/joana)
+
+#### Coders
+
+compress compile . 
+
+#### Hiring corner
+
+Exponea is looking for talented for coders and [AI developers](https://exponea.com/careers/).
+
+#### Credits
+
+Eliza in JS - http://nlp-addiction.com/chatbot/eliza/
+
+Chat UI - http://codepen.io/supah/pen/jqOBqp (MIT licensed)
+
+
---
+++ js/chat.js
@@ -0,0 +1,98 @@
+/*
+Licensed under MIT. Copyright (c) 2016 by Fabio Ottaviani (http://codepen.io/supah/pen/jqOBqp)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 100);
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+  }
+}
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  setDate();
+  var messageReceived =$('.message-input').val();
+  $('.message-input').val(null);
+  updateScrollbar();
+  setTimeout(function() {
+    fakeMessage(messageReceived);
+  }, 1000 + (Math.random() * 20) * 100);
+}
+
+$('.message-submit').click(function() {
+  insertMessage();
+});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
+
+var Fake = [
+  'Hi there, I\'m Joe the Chatbot and you?',
+  'Nice to meet you',
+  'How are you?',
+  'Not too bad, thanks',
+  'What do you do?',
+  'That\'s awesome',
+  'Codepen is a nice place to stay',
+  'I think you\'re a nice person',
+  'Why do you think that?',
+  'Can you explain?',
+  'Anyway I\'ve gotta go now',
+  'It was a pleasure chat with you',
+  'Time to make a new codepen',
+  'Bye',
+  ':)'
+]
+
+function fakeMessage(messageReceived) {
+  //if (messageReceived != '') {
+  //  return false;
+  //}
+  var response_msg = elizaStep(messageReceived);
+
+  $('<div class="message loading new"><figure class="avatar"><img src="img/exponea.ico"/></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+  setTimeout(function() {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><img src="img/exponea.ico" /></figure>' + response_msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    i++;
+  }, 100 + (Math.random() * 20) * 150);
+
+}
